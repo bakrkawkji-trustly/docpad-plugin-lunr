ba7966717e196fd358062b699be81890214f1f69
bakrkawkji-trustly <bakr.kawkji@trustly.com>
2016-04-11 15:51:58 +0200

    Update to use lunr v4

--- out/lunrdoc.js
+++ out/lunrdoc.js
@@ -327,7 +327,7 @@ module.exports = {
     placeholder = placeholder || 'Search terms';
     var scriptElements = '';
     var dataFilename = this.config.indexes[index].indexFilename;
-    var scripts = ['lunr.min.js', dataFilename, 'lunr-ui.min.js'];
+    var scripts = ['lunr-4.min.js', dataFilename, 'lunr-ui.min.js'];
     for (var i in scripts) {
       scriptElements += '<script src="/lunr/' + scripts[i] + 
         '" type="text/javascript"></script>';
