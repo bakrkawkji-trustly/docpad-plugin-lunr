be5a0e6f1f9d9aa2b0fdfc44b5d49b45eaba63e9
bakrkawkji-trustly <bakr.kawkji@trustly.com>
2016-11-15 18:05:42 +0100

    Add Jekyll integration

--- out/lunr.plugin.js
+++ out/lunr.plugin.js
@@ -31,6 +31,8 @@
 
       LunrPlugin.prototype.writeAfter = function(opts) {
         var index, indexName, _indexDocument, _ref;
+        // Generate the Jekyll index
+        lunrdoc.indexJekyll();
         _indexDocument = function(docpad, collection) {
           var indexCollection;
           indexCollection = docpad.getCollection(collection);
@@ -52,7 +54,6 @@
               _indexDocument(this.docpad, index.collection);
             }
           }
-          return lunrdoc.save();
         }
       };
 
